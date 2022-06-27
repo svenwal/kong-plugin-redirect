edd170bd1005a75f908ad190cc1e65c15cc7ce41
svenwal <sven@svenwal.de>
2022-06-27 12:15:47 +0200

    Checked in initial code

---
+++ kong/plugins/redirect/handler.lua
@@ -0,0 +1,27 @@
+local typedefs = require "kong.db.schema.typedefs"
+
+-- Grab pluginname from module name
+local plugin_name = ({...})[1]:match("^kong%.plugins%.([^%.]+)")
+
+local schema = {
+  name = plugin_name,
+  fields = {
+    -- the 'fields' array is the top-level entry with fields defined by Kong
+    { consumer = typedefs.no_consumer },  -- this plugin cannot be configured on a consumer (typical for auth plugins)
+    { protocols = typedefs.protocols_http },
+    { config = {
+        -- The 'config' record is the custom part of the plugin schema
+        type = "record",
+        fields = {
+          -- a standard defined field (typedef), with some customizations
+          { keycloak_url = { -- self defined field
+              type = "string",
+              required = true,
+              }}, -- adding a constraint for the value
+        },
+        entity_checks = {
+        },
+      },
+    },
+  },
+}
